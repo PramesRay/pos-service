583da1932d0ad2b81ce9d4f0305659c3b49a92c2
PramesRay <prames21001@mail.unpad.ac.id>
2025-09-23 14:58:04 +0700

    fix firebase credential configuration

--- app/service/firebase.js
+++ app/service/firebase.js
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_JSON);
 // Inisialisasi Firebase Admin SDK (jika belum diinisialisasi di tempat lain)
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin
